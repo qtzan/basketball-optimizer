1d95ea3af599237c5e3b4b0d00c1ae8f2cdd82fc
Gallections <Gallections@gmail.com>
2025-11-17 22:28:32 -0800

    fix: added error handling with try/finally

--- scripts/webcam_capture.py
+++ scripts/webcam_capture.py
@@ -17,45 +17,46 @@ def main():
     fps = 20.0
     frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    try: 
+        while True:
+            ret, frame = cap.read()
 
-    while True:
-        ret, frame = cap.read()
+            if (not ret):
+                print("Just run into an error capture video frame")
+                return 
+            
+            if writer is not None and is_recording:
+                writer.write(frame)
 
-        if (not ret):
-            print("Just run into an error capture video frame")
-            return 
-        
-        if writer is not None and is_recording:
-            writer.write(frame)
-
-        cv2.imshow("Webcam capture", frame)
-        
-        key = cv2.waitKey(1) & 0XFF
-        if (key == ord("r")):
-            is_recording = True
-            writer = cv2.VideoWriter(
-                output_path, 
-                fourcc, 
-                fps,
-                (frame_width, frame_height)
-            )
-            print("Started Recording ...")
-        elif (key == ord("s")):
-            if is_recording and writer is not None:
-                is_recording = False
-                writer.release()
-                writer = None
-                print("Stopped Recording ...")
-            else:
-                print("Recording has not started yet.")
-        elif (key == ord("q")):
-            print("Quit webcam capture")
-            break
-    cap.release()
-    if writer is not None:
-        writer.release()
-        writer = None
-    cv2.destroyAllWindows()
+            cv2.imshow("Webcam capture", frame)
+            
+            key = cv2.waitKey(1) & 0XFF
+            if (key == ord("r")):
+                is_recording = True
+                writer = cv2.VideoWriter(
+                    output_path, 
+                    fourcc, 
+                    fps,
+                    (frame_width, frame_height)
+                )
+                print("Started Recording ...")
+            elif (key == ord("s")):
+                if is_recording and writer is not None:
+                    is_recording = False
+                    writer.release()
+                    writer = None
+                    print("Stopped Recording ...")
+                else:
+                    print("Recording has not started yet.")
+            elif (key == ord("q")):
+                print("Quit webcam capture")
+                break
+    finally:
+        cap.release()
+        if writer is not None:
+            writer.release()
+            writer = None
+        cv2.destroyAllWindows()
     
 if __name__ == "__main__":
     main()
